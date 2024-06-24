42f8134288cabbd778247de013319a00bfec1c9f
salman165324 <salmansadi165324@gmail.com>
2024-06-24 22:59:57 +0600

    made a small change in the renderblocks

--- src/utils/RenderBlocks.jsx
+++ src/utils/RenderBlocks.jsx
@@ -1,12 +1,15 @@
-import { blocks } from "@/blocks/blocklist";
+import { blocks } from "@/blocks/blockList";
 import React from "react";
 
 export default function RenderBlocks({ layout }) {
-  return <div>{layout.map(block => {
-    const Block = blocks[block.blockTypes]; 
-    if (Block){
-        return <Block key={i} {...block}></Block>; 
-    }
-  })
-  }</div>;
+  return (
+    <div>
+      {layout.map((block, i) => {
+        const Block = blocks[block.blockTypes];
+        if (Block) {
+          return <Block key={i} {...block}></Block>;
+        }
+      })}
+    </div>
+  );
 }
